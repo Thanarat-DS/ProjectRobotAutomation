fcc8992213b826d6bbf1e5fce3b6963c8e7943a1
Thanarat Srihapon <thanarat.sri@trrgroup.com>
2025-01-13 16:12:45 +0700

    map task to function

--- concert_fast.py
+++ concert_fast.py
@@ -11,81 +11,102 @@ import random
 
 # "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="C:\selenium_profile"
 
-price = '฿2,500'
-zone_target = 'A1'
-web_target = "https://www.theconcert.com/concert/3727"
+price = '฿6,940'
+zone_target = 'A2'
+web_target = "https://www.theconcert.com/concert/3775"
 
-chrome_options = Options()
-chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument("--disable-notifications")
-chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36")
+def setup_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36")
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-driver.execute_script("window.navigator.chrome = { runtime: {} };")
-driver.execute_script("Object.defineProperty(navigator, 'languages', {get: () => ['en-US', 'en']});")
-driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3]});")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    driver.execute_script("window.navigator.chrome = { runtime: {} };")
+    driver.execute_script("Object.defineProperty(navigator, 'languages', {get: () => ['en-US', 'en']});")
+    driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3]});")
 
-# Selenium Stealth settings
-stealth(driver,
-      languages=["en-US", "en"],
-      vendor="Google Inc.",
-      platform="Win32",
-      webgl_vendor="Intel Inc.",
-      renderer="Intel Iris OpenGL Engine",
-      fix_hairline=True,
-  )
+    stealth(driver,
+          languages=["en-US", "en"],
+          vendor="Google Inc.",
+          platform="Win32",
+          webgl_vendor="Intel Inc.",
+          renderer="Intel Iris OpenGL Engine",
+          fix_hairline=True,
+      )
 
-## เข้าเว็บ Target & เข้าเลือกที่นั่ง
-driver.get(web_target)
-driver.execute_script("window.scrollBy(0, 80);")
+    return driver
 
-wait = WebDriverWait(driver, 10)
+def open_target_website(driver, url):
+    driver.get(url)
+    driver.execute_script("window.scrollBy(0, 80);")
 
-try:
-    ticket_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="_main-body"]/div/div[1]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div[2]/button')))
-    ticket_button.click()
-except Exception as e:
-    print(f"คลิกปุ่มเข้าซื้อไม่ได้: {e}")
+def enter_ticket_page(driver, wait):
+    try:
+        ticket_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="_main-body"]/div/div[1]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div[2]/button')))
+        ticket_button.click()
+    except Exception as e:
+        print(f"Cannot click ticket button: {e}")
 
-## เลือกโซน
-try:
-    price_button = wait.until(EC.element_to_be_clickable((By.XPATH, f"//a[@class='btn'][contains(text(), '{price}')]")))
-    price_button.click()
-    print(f"clicked price: {price}")
-except Exception as e:
-    print(f"คลิกปุ่มราคาไม่ได้: {e}")
+def select_price(driver, wait, price):
+    try:
+        price_button = wait.until(EC.element_to_be_clickable((By.XPATH, f"//a[@class='btn'][contains(text(), '{price}')]")))
+        price_button.click()
+        print(f"Clicked price: {price}")
+    except Exception as e:
+        print(f"Cannot click price button: {e}")
 
-## เลือกราคา
-try:
-    time.sleep(0.2)
-    zone_items = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.zone-item")))
-    for zone_item in zone_items:
-        try:
-            zone_name = zone_item.find_element(By.CSS_SELECTOR, ".badge.badge-zone").text
-            if zone_name == zone_target:
-                zone_item.click()
-                print(f"clicked zone: {zone_target}")
-                break
-            else:
-                print(zone_name, " != ", zone_target)
-        except Exception as e:
-            print(f"คลิกปุ่มโซนไม่ได้: {e}, zone_name={zone_name}")
-except Exception as e:
-    print(f"Error: {e}")
+def select_zone(driver, wait, zone_target):
+    try:
+        time.sleep(0.2)
+        zone_items = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.zone-item")))
+        for zone_item in zone_items:
+            try:
+                zone_name = zone_item.find_element(By.CSS_SELECTOR, ".badge.badge-zone").text
+                if zone_name == zone_target:
+                    zone_item.click()
+                    print(f"Clicked zone: {zone_target}")
+                    break
+                else:
+                    print(zone_name, " != ", zone_target)
+            except Exception as e:
+                print(f"Cannot click zone: {e}, zone_name={zone_name}")
+    except Exception as e:
+        print(f"Error: {e}")
 
-## คลิกทุกที่นั่ง
-try:
-    time.sleep(0.3)
-    wait_element = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "text[style*='cursor: pointer']")))
-    text_elements = driver.find_elements(By.CSS_SELECTOR, "text[style*='cursor: pointer']")
-    for element in text_elements:
-        element.click()
+def click_all_seats(driver, wait):
+    try:
+        time.sleep(0.3)
+        wait_element = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "text[style*='cursor: pointer']")))
+        text_elements = driver.find_elements(By.CSS_SELECTOR, "text[style*='cursor: pointer']")
+        for element in text_elements:
+            element.click()
+            time.sleep(random.uniform(0.11, 0.15))
+    except Exception as e:
+        print(f"Error or limit reached: {e}")
+    finally:
+        print("Success")
 
-        #หน่วงเวลา
-        time.sleep(random.uniform(0.11, 0.15))
-except Exception as e:
-    print(f"error or may be it reach limit message:{e}")
-finally:
-    print("success")
+if __name__ == "__main__":
+    driver = setup_driver()
+    wait = WebDriverWait(driver, 10)
+
+    # open_target_website(driver, web_target)
+    # enter_ticket_page(driver, wait)
+    # select_price(driver, wait, price)
+    # select_zone(driver, wait, zone_target)
+    # click_all_seats(driver, wait)
+
+    for _ in range(1000):
+        text = int(input("Select Task: \n1: open web\n2: enter ticket page\n3: select price\n4: select zone\n"))
+        match text:
+            case 1:
+                open_target_website(driver, web_target)
+            case 2:
+                enter_ticket_page(driver, wait)
+            case 3:
+                select_price(driver, wait, price)
+            case 4:
+                select_zone(driver, wait, zone_target)
